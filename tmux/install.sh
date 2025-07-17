2907cb5e0b7d1c9363e770a8df6de0916ea3f880
Mosab Ibrahim <mosab.ibrahim@databricks.com>
2025-07-17 10:26:41 +0000

    Add tpm plugin manager to tmux installer

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,7 +2,7 @@
 set -eux
 set -o pipefail
 
-TMUX_CONFIG_PATH=~/workspace/dotfiles/tmux/tmux.conf
+TMUX_CONFIG_PATH=~/dotfiles/tmux/tmux.conf
 SYMLINK_PATH=~/.tmux.conf
 if [ ! -f $TMUX_CONFIG_PATH ]; then
   echo "config file does not exist" && exit
@@ -14,3 +14,5 @@ if [ -L ${SYMLINK_PATH} ]; then
 fi
 
 ln -s ${TMUX_CONFIG_PATH} ${SYMLINK_PATH}
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
