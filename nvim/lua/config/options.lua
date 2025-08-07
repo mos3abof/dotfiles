7f6d01c45841086a52055c167f74d05a3e1ef9be
Mosab Ibrahim <mosab.a.ibrahim@gamil.com>
2025-08-07 18:51:30 +0000

    Saving changes

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -20,3 +20,6 @@ vim.opt.swapfile = false
 -- only for diagnostics. The rest of LSP support will still be
 -- provided by rust-analyzer.
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+
+
+vim.g.jsonnet_fmt_options = "--string-style d"
